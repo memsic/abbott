fdeec9bee464242b947e3667b953692b8a4edc9d
Brett Memsic <memsic@gmail.com>
2013-03-26 18:12:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abbott::Application.config.secret_token = '0d61baa6bfee9f753dcc20da52c485937deb4929cf7b81dc085b4514e04a40e8622e274e3697424d78a93ddc05c23e500aae036199336d05af716e4c6102a03d'
